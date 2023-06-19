010998b9b868f81bd9a2de6f91e8109b9ff74647
patteesolomon <patteesolomon@gmail.com>
2023-06-18 23:29:31 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <p>Hello</p>
+      <p>Welcome To Apotheosis</p>
+      <iframe width="985" height="554" src="https://www.youtube.com/embed/wnHn6KKt1QY" title="Mega Man X5 OST - Zero Stage 1 [EXTENDED]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </>
+  )
+}
+
+export default App
